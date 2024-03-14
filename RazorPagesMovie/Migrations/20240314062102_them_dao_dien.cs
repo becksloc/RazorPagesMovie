de5d6244bf2584364def01a79a26fcd5b8a20b20
Loc NGUYEN <nguyentheloc@gmail.com>
2024-03-14 14:57:40 +0700

    sửa UI và model

---
+++ RazorPagesMovie/Migrations/20240314062102_them_dao_dien.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class them_dao_dien : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Movie",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Movie");
+        }
+    }
+}
--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,14 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [DisplayName("Tên phim")]
+        [Required(ErrorMessage = "Không được bỏ trống")]
         public string? Title { get; set; }
+        
+        [DisplayName("Ngày phát hành")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [DisplayName("Đạo diễn")]
+        [StringLength(50)]
+        public string? Director { get; set; }
+
+        [DisplayName("Thể loại")]
         public string? Genre { get; set; }
+
+        [DisplayName("Giá tiền")]
+        [Required(ErrorMessage = "Không được bỏ trống và phải là số")]
         public decimal Price { get; set; }
     }
 }
